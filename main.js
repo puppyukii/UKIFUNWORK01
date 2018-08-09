047de0dd225b9155ba9de8d36fc7d943ceed4421
puppyukii <babibabitha53@gmail.com>
2018-08-09 21:09:12 +0530

    soundcloud

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
-//album song
+
 $(document).ready(function() {
   SC.stream('/tracks/267806421',function(sound){
     $('#start').click(function(e) {
@@ -13,7 +13,7 @@ $(document).ready(function() {
            sound.stop();
         });
   });
-//thiran
+
  SC.stream('/tracks/340155138',function(sound){
     $('#start1').click(function(e) {
            e.preventDefault();
@@ -26,7 +26,7 @@ $(document).ready(function() {
   });
 
 
-//sangamam
+
   SC.stream('/tracks/362640818',function(sound){
     $('#start2').click(function(e) {
            e.preventDefault();
@@ -39,7 +39,7 @@ $(document).ready(function() {
   });
 
 
-//appuchsikiramam
+
   SC.stream('/tracks/256119085',function(sound){
     $('#start3').click(function(e) {
            e.preventDefault();
@@ -52,7 +52,7 @@ $(document).ready(function() {
   });
 
 
-//keethaiyen rathai
+
   SC.stream('/tracks/206493559',function(sound){
     $('#start4').click(function(e) {
            e.preventDefault();
@@ -65,7 +65,7 @@ $(document).ready(function() {
   });
 
 
-//enai nokki paum thodda
+
   SC.stream('/tracks/291358852',function(sound){
     $('#start5').click(function(e) {
            e.preventDefault();
@@ -76,7 +76,7 @@ $(document).ready(function() {
            sound.stop();
         });
   });
-//thodare
+
   SC.stream('/tracks/311739188',function(sound){
     $('#start6').click(function(e) {
            e.preventDefault();
@@ -87,7 +87,7 @@ $(document).ready(function() {
            sound.stop();
         });
   });
-//rekka
+
   SC.stream('/tracks/311971479',function(sound){
     $('#start7').click(function(e) {
            e.preventDefault();
@@ -98,7 +98,7 @@ $(document).ready(function() {
            sound.stop();
         });
   });
-//mersal
+
   SC.stream('/tracks/225519099',function(sound){
     $('#start8').click(function(e) {
            e.preventDefault();
@@ -109,7 +109,7 @@ $(document).ready(function() {
            sound.stop();
         });
   });
-//thas
+
   SC.stream('/tracks/315401304',function(sound){
     $('#start9').click(function(e) {
            e.preventDefault();
@@ -121,7 +121,7 @@ $(document).ready(function() {
         });
   });
 
-//desinguraja
+
 SC.stream('/tracks/36358909',function(sound){
   $('#start10').click(function(e) {
          e.preventDefault();
@@ -133,7 +133,7 @@ SC.stream('/tracks/36358909',function(sound){
       });
 });
 
-//leelai
+
 SC.stream('/tracks/407128221',function(sound){
   $('#start11').click(function(e) {
          e.preventDefault();
